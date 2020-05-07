6827ea7bd8de2008b8d3c9a1e0cfb57425285937
Jayanth Srivatsav <maneshjay@gmail.com>
2020-05-08 01:01:00 +0530

    Secrets app complete

--- Express/Wiki-API/app.js
+++ Express/Wiki-API/app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.set("view-engine", ejs);
+app.set("view-engine", 'ejs');
 app.set(express.static("static-files"));
 
 mongoose.connect("mongodb://localhost:27017/wikiDB", {
